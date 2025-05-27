065b685e1636dae6bfec7aa06626b5d93ac5cfa7
chicken8848 <mech_4e@hotmail.com>
2025-05-27 15:55:40 +0800

    lexer
    TODO: whitespaces

---
+++ src/monkey/lexer/lexer.go
@@ -0,0 +1,88 @@
+package lexer
+
+import (
+	"monkey/token"
+	"unicode/utf8"
+)
+
+type Lexer struct {
+	input string
+	// current position in input (points to current char)
+	position int
+	// current reading position in input (after current char)
+	readPosition int
+	// current char under examination
+	ch rune
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	}
+	r, width := utf8.DecodeRuneInString(l.input[l.readPosition:])
+	l.ch = r
+	l.position = l.readPosition
+	l.readPosition += width
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case '?':
+		tok = newToken(token.QMARK, l.ch)
+	case '!':
+		tok = newToken(token.BANG, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+	l.readChar()
+	return tok
+}
+
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func isLetter(ch rune) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func newToken(tokenType token.TokenType, ch rune) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
